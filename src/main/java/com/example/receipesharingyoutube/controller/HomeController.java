ed88a64cd1767a2e8becad0cda76e81e04491f98
DilkaSithari <sitharidilka@gmail.com>
2024-02-03 13:27:02 +0530

    First Commit

--- src/main/java/com/example/receipesharingyoutube/controller/HomeController.java
+++ src/main/java/com/example/receipesharingyoutube/controller/HomeController.java
@@ -1,8 +1,11 @@
-package controller;
+package com.example.receipesharingyoutube.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
+
     @GetMapping
     public String homeController(){
         return "Hiiii";
