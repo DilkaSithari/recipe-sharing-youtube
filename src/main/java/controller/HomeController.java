5fc00c75564689f60ab1a758ca4d05258b105c52
DilkaSithari <sitharidilka@gmail.com>
2024-02-03 13:17:46 +0530

    First Commit

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+    @GetMapping
+    public String homeController(){
+        return "Hiiii";
+    }
 
 }
